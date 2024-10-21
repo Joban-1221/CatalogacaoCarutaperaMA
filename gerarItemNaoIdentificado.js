60e1d0fdd2f5d5bb64ad5d1a527085d532224a13
Joban1221 <Jovan.louzeiro@gmail.com>
2024-10-20 22:31:19 -0300

    Versão SNCT OFC - Incompleta

--- gerarItemNaoIdentificado.js
+++ gerarItemNaoIdentificado.js
@@ -1,5 +1,5 @@
 
-for (let i = 1; i <= 34; i++) {
+for (let i = 1; i <= 33; i++) {
     especieNaoIdentificada = {
         modeloId: `modeloId${i}`,
         imgSrc: `imgs/especies/Não identificado/Achar especie ${i + 1}/1.jpg`,
