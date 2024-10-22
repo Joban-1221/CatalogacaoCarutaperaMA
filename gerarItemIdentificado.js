18e6274083fd50ea5c755d5c0f615d8dc90ca308
Joban1221 <Jovan.louzeiro@gmail.com>
2024-10-22 10:00:07 -0300

    Arrumando bug da ordem

--- gerarItemIdentificado.js
+++ gerarItemIdentificado.js
@@ -1,22 +1,28 @@
-let especiesCatalogo = []
-fetch("catalogacaoDados.xlsx").then(response => response.arrayBuffer()).then(data => {
-    const workbook = XLSX.read(data, { type: 'array' });
+async function gerarItensIdentificados() {
+    let especiesCatalogo = []
+    fetch("catalogacaoDados.xlsx").then(response => response.arrayBuffer()).then(data => {
+        const workbook = XLSX.read(data, { type: 'array' });
 
-    // Obtém a primeira planilha
-    const firstSheet = workbook.Sheets[workbook.SheetNames[0]];
+        // Obtém a primeira planilha
+        const firstSheet = workbook.Sheets[workbook.SheetNames[0]];
 
-    // Converte a planilha para JSON
-    especiesCatalogo = XLSX.utils.sheet_to_json(firstSheet);
+        // Converte a planilha para JSON
+        especiesCatalogo = XLSX.utils.sheet_to_json(firstSheet);
 
-    console.log(especiesCatalogo)
+        asyncForEachCatalogo(especiesCatalogo)
+        console.log(especiesCatalogo)
 
-    especiesCatalogo.forEach(element => {
-        gerarCatalogo(element)
-    });
-})
-    .catch(error => {
-        console.error('Erro ao carregar o arquivo Excel:', error);
-    });
+    })
+        .catch(error => {
+            console.error('Erro ao carregar o arquivo Excel:', error);
+        });
+}
+
+async function asyncForEachCatalogo(array) {
+    for (let index = 0; index < array.length; index++) {
+      await gerarCatalogo(array[index]);
+    }
+  }
 
 function gerarId(modeloId, nomeElemento) {
     const id = `${nomeElemento}${modeloId}`
@@ -444,4 +450,10 @@ async function gerarCatalogoNaoIdentificado(especie) {
 
     //Adicionar o item ao catalogo
     todosCatalogos.appendChild(divGridCatalogo)
+}
+
+if(window.location.pathname === '/' || window.location.pathname === '/index.html'){
+    gerarItensIdentificados()
+}else if(window.location.pathname === 'especiesNaoIdentificadas'){
+    gerarCatalogoNaoIdentificado()
 }
